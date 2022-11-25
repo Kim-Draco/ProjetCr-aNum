3caa99b3e12aa66476957b712720eaf76fb7b348
Camzzzzz <94546887+Camzzzzz@users.noreply.github.com>
2022-11-25 19:48:35 +0100

    ligne + treble

--- Notes.py
+++ Notes.py
@@ -1,11 +1,14 @@
-from asyncio import events
+# from asyncio import events
 import pygame
-from tuner import Tuner
+# from tuner import Tuner
 
 class JOUENOTE():
 
     def __init__(self, screen, carryOn):
-
+        self.bar = pygame.image.load('images/Symbols/Bar_lines.png')
+        self.bar = pygame.transform.scale(self.bar, (1290, 520))
+        self.treble = pygame.image.load('images/Symbols/Treble.png')
+        self.treble = pygame.transform.scale(self.treble, (580, 620))
         # The loop will carry on until the user exits the game (e.g. clicks the close button).
         carryOnThis = True
         stopGame = False
@@ -16,21 +19,27 @@ class JOUENOTE():
         while carryOnThis and carryOn:
             for event in pygame.event.get(): # User did something
                 if event.type == pygame.QUIT: # If user clicked close
-                    pygame.quit() # Flag that we are done, so we can exit the while loop
+                    pygame.quit()
+                    # Flag that we are done, so we can exit the while loop
                 elif event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_ESCAPE:
                         carryOnThis = False
-                        
+
             screen.fill([150,150,150])
+            screen.fill([150, 150, 150])
+            screen.blit(self.bar, (0, 0))
+            screen.blit(self.treble, (-200, 60))
 
             # --- Go ahead and update the screen with what we've drawn.
             pygame.display.flip()
 
+
+
             # Charge tuner
             # Ne fonctionne pas pour l'instant plus rien ne fonctionne pendant l'écoute
-            tuner = Tuner()
+            #tuner = Tuner()
             # Start listening to the notes played
-            tuner.musique()
+            #tuner.musique()
             
             # --- Limit to 60 frames per second
             clock.tick(60)
