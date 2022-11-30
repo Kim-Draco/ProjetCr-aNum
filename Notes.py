8eaea79fc15abfbd7405428ee7f3416b4da212f3
Kim-Brice <kbrice8th@gmail.com>
2022-11-30 10:08:59 +0100

    feat: son des notes

--- Notes.py
+++ Notes.py
@@ -48,6 +48,7 @@ class JOUENOTE():
         self.do4_rect.y = 480
         self.do4_pressed = pygame.image.load('images/Button/do4pressed.png')
         self.do4_pressed = pygame.transform.scale(self.do4_pressed, (180, 130))
+        self.do4sound = pygame.mixer.Sound("piano-mp3/C4.wav")
 
         self.do5 = pygame.image.load('images/Button/do5.png')
         self.do5 = pygame.transform.scale(self.do5, (180, 130))
@@ -56,7 +57,8 @@ class JOUENOTE():
         self.do5_rect.y = 580
         self.do5_pressed = pygame.image.load('images/Button/do5pressed.png')
         self.do5_pressed = pygame.transform.scale(self.do5_pressed, (180, 130))
-
+        self.do5sound = pygame.mixer.Sound("piano-mp3/C5.wav")
+        
         self.fa4 = pygame.image.load('images/Button/fa4.png')
         self.fa4 = pygame.transform.scale(self.fa4, (180, 130))
         self.fa4_rect = self.fa4.get_rect()
@@ -64,7 +66,8 @@ class JOUENOTE():
         self.fa4_rect.y = 580
         self.fa4_pressed = pygame.image.load('images/Button/fa4pressed.png')
         self.fa4_pressed = pygame.transform.scale(self.fa4_pressed, (180, 130))
-
+        self.fa4sound = pygame.mixer.Sound("piano-mp3/F4.wav")
+        
         self.fa5 = pygame.image.load('images/Button/fa5.png')
         self.fa5 = pygame.transform.scale(self.fa5, (180, 130))
         self.fa5_rect = self.fa5.get_rect()
@@ -72,7 +75,8 @@ class JOUENOTE():
         self.fa5_rect.y = 480
         self.fa5_pressed = pygame.image.load('images/Button/fa5pressed.png')
         self.fa5_pressed = pygame.transform.scale(self.fa5_pressed, (180, 130))
-
+        self.fa5sound = pygame.mixer.Sound("piano-mp3/F5.wav")
+        
         self.fasharp4 = pygame.image.load('images/Button/fasharp4.png')
         self.fasharp4 = pygame.transform.scale(self.fasharp4, (180, 130))
         self.fasharp4_rect = self.fasharp4.get_rect()
@@ -80,7 +84,8 @@ class JOUENOTE():
         self.fasharp4_rect.y = 360
         self.fasharp4_pressed = pygame.image.load('images/Button/fasharp4pressed.png')
         self.fasharp4_pressed = pygame.transform.scale(self.fasharp4_pressed, (180, 130))
-
+        self.fasharp4sound = pygame.mixer.Sound("piano-mp3/Gb4.wav")
+        
         self.fasharp5 = pygame.image.load('images/Button/fasharp5.png')
         self.fasharp5 = pygame.transform.scale(self.fasharp5, (180, 130))
         self.fasharp5_rect = self.fasharp5.get_rect()
@@ -88,7 +93,8 @@ class JOUENOTE():
         self.fasharp5_rect.y = 360
         self.fasharp5_pressed = pygame.image.load('images/Button/fasharp5pressed.png')
         self.fasharp5_pressed = pygame.transform.scale(self.fasharp5_pressed, (180, 130))
-
+        self.fasharp5sound = pygame.mixer.Sound("piano-mp3/Gb5.wav")
+        
         self.la4 = pygame.image.load('images/Button/la4.png')
         self.la4 = pygame.transform.scale(self.la4, (180, 130))
         self.la4_rect = self.la4.get_rect()
@@ -96,7 +102,8 @@ class JOUENOTE():
         self.la4_rect.y = 580
         self.la4_pressed = pygame.image.load('images/Button/la4pressed.png')
         self.la4_pressed = pygame.transform.scale(self.la4_pressed, (180, 130))
-
+        self.la4sound = pygame.mixer.Sound("piano-mp3/A4.wav")
+        
         self.la5 = pygame.image.load('images/Button/la5.png')
         self.la5 = pygame.transform.scale(self.la5, (180, 130))
         self.la5_rect = self.la5.get_rect()
@@ -104,7 +111,8 @@ class JOUENOTE():
         self.la5_rect.y = 480
         self.la5_pressed = pygame.image.load('images/Button/la5pressed.png')
         self.la5_pressed = pygame.transform.scale(self.la5_pressed, (180, 130))
-
+        self.la5sound = pygame.mixer.Sound("piano-mp3/A5.wav")
+        
         self.mi4 = pygame.image.load('images/Button/mi4.png')
         self.mi4 = pygame.transform.scale(self.mi4, (180, 130))
         self.mi4_rect = self.mi4.get_rect()
@@ -112,7 +120,8 @@ class JOUENOTE():
         self.mi4_rect.y = 480
         self.mi4_pressed = pygame.image.load('images/Button/mi4pressed.png')
         self.mi4_pressed = pygame.transform.scale(self.mi4_pressed, (180, 130))
-
+        self.mi4sound = pygame.mixer.Sound("piano-mp3/E4.wav")
+        
         self.mi5 = pygame.image.load('images/Button/mi5.png')
         self.mi5 = pygame.transform.scale(self.mi5, (180, 130))
         self.mi5_rect = self.mi5.get_rect()
@@ -120,7 +129,8 @@ class JOUENOTE():
         self.mi5_rect.y = 580
         self.mi5_pressed = pygame.image.load('images/Button/mi5pressed.png')
         self.mi5_pressed = pygame.transform.scale(self.mi5_pressed, (180, 130))
-
+        self.mi5sound = pygame.mixer.Sound("piano-mp3/E5.wav")
+        
         self.re4 = pygame.image.load('images/Button/re4.png')
         self.re4 = pygame.transform.scale(self.re4, (180, 130))
         self.re4_rect = self.re4.get_rect()
@@ -128,7 +138,8 @@ class JOUENOTE():
         self.re4_rect.y = 580
         self.re4_pressed = pygame.image.load('images/Button/re4pressed.png')
         self.re4_pressed = pygame.transform.scale(self.re4_pressed, (180, 130))
-
+        self.re4sound = pygame.mixer.Sound("piano-mp3/D4.wav")
+        
         self.re5 = pygame.image.load('images/Button/re5.png')
         self.re5 = pygame.transform.scale(self.re5, (180, 130))
         self.re5_rect = self.re5.get_rect()
@@ -136,7 +147,8 @@ class JOUENOTE():
         self.re5_rect.y = 480
         self.re5_pressed = pygame.image.load('images/Button/re5pressed.png')
         self.re5_pressed = pygame.transform.scale(self.re5_pressed, (180, 130))
-
+        self.re5sound = pygame.mixer.Sound("piano-mp3/D5.wav")
+        
         self.si4 = pygame.image.load('images/Button/si4.png')
         self.si4 = pygame.transform.scale(self.si4, (180, 130))
         self.si4_rect = self.si4.get_rect()
@@ -144,7 +156,8 @@ class JOUENOTE():
         self.si4_rect.y = 480
         self.si4_pressed = pygame.image.load('images/Button/si4pressed.png')
         self.si4_pressed = pygame.transform.scale(self.si4_pressed, (180, 130))
-
+        self.si4sound = pygame.mixer.Sound("piano-mp3/B4.wav")
+        
         self.si5 = pygame.image.load('images/Button/si5.png')
         self.si5 = pygame.transform.scale(self.si5, (180, 130))
         self.si5_rect = self.si5.get_rect()
@@ -152,7 +165,8 @@ class JOUENOTE():
         self.si5_rect.y = 580
         self.si5_pressed = pygame.image.load('images/Button/si5pressed.png')
         self.si5_pressed = pygame.transform.scale(self.si5_pressed, (180, 130))
-
+        self.si5sound = pygame.mixer.Sound("piano-mp3/B5.wav")
+        
         self.sol4 = pygame.image.load('images/Button/sol4.png')
         self.sol4 = pygame.transform.scale(self.sol4, (180, 130))
         self.sol4_rect = self.sol4.get_rect()
@@ -160,7 +174,8 @@ class JOUENOTE():
         self.sol4_rect.y = 480
         self.sol4_pressed = pygame.image.load('images/Button/sol4pressed.png')
         self.sol4_pressed = pygame.transform.scale(self.sol4_pressed, (180, 130))
-
+        self.sol4sound = pygame.mixer.Sound("piano-mp3/G4.wav")
+        
         self.sol5 = pygame.image.load('images/Button/sol5.png')
         self.sol5 = pygame.transform.scale(self.sol5, (180, 130))
         self.sol5_rect = self.sol5.get_rect()
@@ -168,6 +183,15 @@ class JOUENOTE():
         self.sol5_rect.y = 580
         self.sol5_pressed = pygame.image.load('images/Button/sol5pressed.png')
         self.sol5_pressed = pygame.transform.scale(self.sol5_pressed, (180, 130))
+        self.sol5sound = pygame.mixer.Sound("piano-mp3/G5.wav")
+        
+        self.play = pygame.image.load('images/Button/play.png')
+        self.play = pygame.transform.scale(self.play, (180, 130))
+        self.play_rect = self.play.get_rect()
+        self.play_rect.x = 550
+        self.play_rect.y = 20
+        self.play_pressed = pygame.image.load('images/Button/playpressed.png')
+        self.play_pressed = pygame.transform.scale(self.play_pressed, (180, 130))
 
         mixer.init()
         self.zelda = pygame.mixer.Sound("music/ZeldasLullaby.wav")
@@ -205,8 +229,8 @@ class JOUENOTE():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         carryOnThis = False
-                if not instrument:
-                    if event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN:    
+                    if not instrument:
                         if self.do4_rect.collidepoint(event.pos):
                             self.button = 1
                         elif self.re4_rect.collidepoint(event.pos):
@@ -267,86 +291,107 @@ class JOUENOTE():
                             self.button = 15
                         elif self.fasharp5_rect.collidepoint(event.pos):
                             self.button = 16
+                    if self.play_rect.collidepoint(event.pos):
+                        self.button = 17
+                        self.zelda.stop()
+                        self.zelda.play()
+
 
             if not instrument:
                 # Display the button
                 if self.button == 1:
                     screen.blit(self.do4_pressed, self.do4_rect)
+                    self.do4sound.play()
                 else:
                     screen.blit(self.do4, self.do4_rect)
 
                 if self.button == 2:
                     screen.blit(self.re4_pressed, self.re4_rect)
+                    self.re4sound.play()
                 else:
                     screen.blit(self.re4, self.re4_rect)
 
                 if self.button == 3:
                     screen.blit(self.mi4_pressed, self.mi4_rect)
+                    self.mi4sound.play()
                 else:
                     screen.blit(self.mi4, self.mi4_rect)
 
                 if self.button == 4:
                     screen.blit(self.fa4_pressed, self.fa4_rect)
+                    self.fa4sound.play()
                 else:
                     screen.blit(self.fa4, self.fa4_rect)
 
                 if self.button == 5:
                     screen.blit(self.sol4_pressed, self.sol4_rect)
+                    self.sol4sound.play()
                 else:
                     screen.blit(self.sol4, self.sol4_rect)
 
                 if self.button == 6:
                     screen.blit(self.la4_pressed, self.la4_rect)
+                    self.la4sound.play()
                 else:
                     screen.blit(self.la4, self.la4_rect)
 
                 if self.button == 7:
                     screen.blit(self.si4_pressed, self.si4_rect)
+                    self.si4sound.play()
                 else:
                     screen.blit(self.si4, self.si4_rect)
 
                 if self.button == 8:
                     screen.blit(self.do5_pressed, self.do5_rect)
+                    self.do5sound.play()
                 else:
                     screen.blit(self.do5, self.do5_rect)
 
                 if self.button == 9:
                     screen.blit(self.re5_pressed, self.re5_rect)
+                    self.re5sound.play()
                 else:
                     screen.blit(self.re5, self.re5_rect)
 
                 if self.button == 10:
                     screen.blit(self.mi5_pressed, self.mi5_rect)
+                    self.mi5sound.play()
                 else:
                     screen.blit(self.mi5, self.mi5_rect)
 
                 if self.button == 11:
                     screen.blit(self.fa5_pressed, self.fa5_rect)
+                    self.fa5sound.play()
                 else:
                     screen.blit(self.fa5, self.fa5_rect)
 
                 if self.button == 12:
                     screen.blit(self.sol5_pressed, self.sol5_rect)
+                    self.sol5sound.play()
                 else:
                     screen.blit(self.sol5, self.sol5_rect)
 
                 if self.button == 13:
                     screen.blit(self.la5_pressed, self.la5_rect)
+                    self.la5sound.play()
                 else:
                     screen.blit(self.la5, self.la5_rect)
 
                 if self.button == 14:
                     screen.blit(self.si5_pressed, self.si5_rect)
+                    self.si5sound.play()
                 else:
                     screen.blit(self.si5, self.si5_rect)
 
                 if self.button == 15:
                     screen.blit(self.fasharp4_pressed, self.fasharp4_rect)
+                    self.fasharp4sound.play()
                 else:
                     screen.blit(self.fasharp4, self.fasharp4_rect)
 
                 if self.button == 16:
                     screen.blit(self.fasharp5_pressed, self.fasharp5_rect)
+                    self.fasharp5sound.play()
                 else:
                     screen.blit(self.fasharp5, self.fasharp5_rect)
 
@@ -453,6 +498,10 @@ class JOUENOTE():
                 # Start listening to the notes played
                 carryOnThis = tuner.musique(carryOn, carryOnThis, screen)
 
+            if self.button == 17:
+                screen.blit(self.play_pressed, self.play_rect)
+            else:
+                screen.blit(self.play, self.play_rect)
             # Update the screen with what we've drawn.
             pygame.display.flip()
 
---
+++ PygameTest.py
@@ -0,0 +1,119 @@
+from tkinter import CENTER
+import pygame, random
+from Menu import MENU, CREA_MENU
+from Colors import COLORS
+from Notes import JOUENOTE
+pygame.init()
+
+size = (1280,720)
+screen = pygame.display.set_mode(size)
+background = pygame.image.load("images/PageAcceuil/AccueilPLay.png")
+background = pygame.transform.scale(background, (1280, 720))
+
+index_list_Selections = CREA_MENU.index_list_Selections
+RectMenu_list_Selections = CREA_MENU.RectMenu_list_Selections
+list_Selections = CREA_MENU.list_Selections
+NbMenu = CREA_MENU.NbMenu
+Selection = CREA_MENU.Selection
+LeftMenu = True
+TestMenu = pygame.Surface((155,180))
+TestMenu.set_alpha(160)
+TestMenu.fill((255,0,0))
+
+pygame.display.set_caption("Mon Jeu")
+
+
+for i in RectMenu_list_Selections:
+    list_Selections.add(i)
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
+carryOn = True
+ 
+# The clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+
+
+
+# -------- Main Program Loop -----------
+while carryOn:
+    # --- Main event loop
+    
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+              carryOn = False # Flag that we are done so we can exit the while loop
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT or event.key==pygame.K_LEFT:
+                if LeftMenu == True:
+                    Selection = 1
+                    LeftMenu = False
+                else:
+                    Selection = 0
+                    LeftMenu = True
+                    
+            if event.key==pygame.K_DOWN and LeftMenu == False:
+                if Selection < NbMenu:
+                    Selection += 1                    
+                else:
+                    Selection = 1
+            if event.key==pygame.K_UP and LeftMenu == False:
+                if Selection > 1:
+                    Selection -= 1                    
+                else:
+                    Selection = NbMenu
+
+            if event.key==pygame.K_RETURN:
+                if Selection == NbMenu:
+                    carryOn=False
+                elif Selection == 0:
+                    JOUENOTE(screen, carryOn)
+
+        # Met à jour en fonction de la position de la souris
+        elif event.type == pygame.MOUSEMOTION:
+            if pygame.mouse.get_pos()[0] >= 350 and pygame.mouse.get_pos()[0] <= 550 and pygame.mouse.get_pos()[1] >= 110 and pygame.mouse.get_pos()[1] <= 670:
+                Selection = 0
+                LeftMenu = True
+
+            if pygame.mouse.get_pos()[0] >= 760 and pygame.mouse.get_pos()[0] <= 915 and pygame.mouse.get_pos()[1] >= 110 and pygame.mouse.get_pos()[1] <= 270:
+                Selection = 1
+                LeftMenu = False
+
+            if pygame.mouse.get_pos()[0] >= 760 and pygame.mouse.get_pos()[0] <= 915 and pygame.mouse.get_pos()[1] >= 290 and pygame.mouse.get_pos()[1] <= 470:
+                Selection = 2
+                LeftMenu = False
+
+            if pygame.mouse.get_pos()[0] >= 760 and pygame.mouse.get_pos()[0] <= 915 and pygame.mouse.get_pos()[1] >= 490 and pygame.mouse.get_pos()[1] <= 670:
+                Selection = 3
+                LeftMenu = False
+
+        #gère le click de la souris
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if Selection == NbMenu and event.button == 1:
+                carryOn=False
+            elif Selection == 0:
+                JOUENOTE(screen, carryOn)
+
+    # --- Game logic should go here
+
+    for i in index_list_Selections:
+        if i == index_list_Selections[Selection]:
+            background = index_list_Selections[Selection]
+
+    # --- Drawing code should go here
+    # First, clear the screen to white. 
+    # screen.fill([150,150,150])
+    screen.blit(background, (0, 0))
+    # screen.blit(TestMenu, (760, 490))
+    # The you can draw different shapes and lines or add text to your background stage.
+    # coordonné : x, y, taille x, taille y !!!!
+    # list_Selections.draw(screen)
+    #pygame.draw.rect(screen, GREEN, [100, 225, 100, 50],0)
+    #pygame.draw.rect(screen, [255,255,0], [300, 225, 100, 50],0)
+ 
+     # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+     
+     # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+#Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
